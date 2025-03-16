52f47b1f0a9343d078089e380e626180c8316853
Benjamin Jørgensen <me@benmi.me>
2025-03-16 21:19:32 +0900

    now has working quicked! (also removed macos folder trash file)

--- crates/services/web-server/examples/quick_dev.rs
+++ crates/services/web-server/examples/quick_dev.rs
@@ -1,9 +1,7 @@
 #![allow(unused)] // For example code.
 
-pub type Result<T> = core::result::Result<T, Error>;
-pub type Error = Box<dyn std::error::Error>; // For examples.
-
-use serde_json::{json, Value};
+use anyhow::Result;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -11,95 +9,175 @@ async fn main() -> Result<()> {
 
 	// hc.do_get("/index.html").await?.print().await?;
 
-	// -- Login
 	let req_login = hc.do_post(
 		"/api/login",
 		json!({
 			"username": "demo1",
-			"pwd": "welcome"
+			"pwd": "supersecretpassword"
 		}),
 	);
 	req_login.await?.print().await?;
 
-	// -- Create Agent
-	let req_create_agent = hc.do_post(
+	let req_create_project = hc.do_post(
 		"/api/rpc",
 		json!({
-			"jsonrpc": "2.0",
+		"jsonrpc": "2.0",
 			"id": 1,
-			"method": "create_agent",
+			"method": "create_project",
 			"params": {
 				"data": {
-					"name": "agent AAA"
+					"name": "project AAA"
 				}
 			}
 		}),
 	);
-	let result = req_create_agent.await?;
+	let result = req_create_project.await?;
 	result.print().await?;
-	let agent_id = result.json_value::<i64>("/result/data/id")?;
+	let project_id = result.json_value::<i64>("/result/data/id")?;
 
-	// -- Get Agent
-	let req_get_agent = hc.do_post(
+	let mut task_ids: Vec<i64> = Vec::new();
+	for i in 1..=5 {
+		let req_create_task = hc.do_post(
+			"/api/rpc",
+			json!({
+			"jsonrpc": "2.0",
+				  "id": 1,
+				  "method": "create_task",
+				  "params": {
+					  "data": {
+						  "project_id": project_id,
+						  "title": format!("task AAA {i}")
+					  }
+				  }
+			  }),
+		);
+		let result = req_create_task.await?;
+		task_ids.push(result.json_value::<i64>("/result/data/id")?);
+	}
+
+	let req_update_task = hc.do_post(
 		"/api/rpc",
 		json!({
-			"jsonrpc": "2.0",
+		"jsonrpc": "2.0",
 			"id": 1,
-			"method": "get_agent",
+			"method": "update_task",
 			"params": {
-					"id": agent_id
-			}
-		}),
-	);
-	let result = req_get_agent.await?;
-	result.print().await?;
-
-	// -- Create Conv
-	let req_create_conv = hc.do_post(
-		"/api/rpc",
-		json!({
-			"jsonrpc": "2.0",
-			"id": 1,
-			"method": "create_conv",
-			"params": {
+				"id": task_ids[0], // The first task created.
 				"data": {
-					"agent_id": agent_id,
-					"title": "conv 01"
+					"title": "task BB"
 				}
 			}
 		}),
 	);
-	let result = req_create_conv.await?;
-	result.print().await?;
-	let conv_id = result.json_value::<i64>("/result/data/id")?;
+	req_update_task.await?.print().await?;
 
-	// -- Create ConvMsg
-	let req_create_conv = hc.do_post(
+	let mut timerecord_ids: Vec<i64> = Vec::new();
+	for i in 1..=5 {
+		let req_create_timerecords = hc.do_post(
+			"/api/rpc",
+			json!({
+			"jsonrpc": "2.0",
+				  "id": 1,
+				  "method": "create_timerecord",
+				  "params": {
+					  "data": {
+						  "task_id": task_ids[1],
+						  "start_time": "2024-05-02T13:40:39.635577783Z",
+						  "stop_time": "2024-05-03T13:40:39.635577783Z",
+						  "place": format!("place CCC {i}")
+					  }
+				  }
+			  }),
+		);
+		let result = req_create_timerecords.await?;
+		timerecord_ids.push(result.json_value::<i64>("/result/data/id")?);
+	}
+
+	let req_list_all_timerecords = hc.do_post(
 		"/api/rpc",
 		json!({
-			"jsonrpc": "2.0",
+		"jsonrpc": "2.0",
 			"id": 1,
-			"method": "add_conv_msg",
+			"method": "list_timerecords",
 			"params": {
-				"data": {
-					"conv_id": conv_id,
-					"content": "This is the first comment"
+				"filters": {
+					"task_id": task_ids[1]
+				},
+				"list_options": {
+					"order_bys": "!place"
 				}
 			}
 		}),
 	);
-	let result = req_create_conv.await?;
-	result.print().await?;
-	let conv_msg_id = result.json_value::<i64>("/result/data/id")?;
+	req_list_all_timerecords.await?.print().await?;
 
-	// -- Logoff
+	let req_delete_task = hc.do_post(
+		"/api/rpc",
+		json!({
+		"jsonrpc": "2.0",
+			"id": 1,
+			"method": "delete_task",
+			"params": {
+				"id": task_ids[1] // The second task created.
+			}
+		}),
+	);
+	req_delete_task.await?.print().await?;
+
+	let req_list_all_tasks = hc.do_post(
+		"/api/rpc",
+		json!({
+		"jsonrpc": "2.0",
+			"id": 1,
+			"method": "list_tasks",
+			"params": {
+				"filters": {
+					"project_id": project_id
+				},
+				"list_options": {
+					"order_bys": "!title"
+				}
+			}
+		}),
+	);
+	req_list_all_tasks.await?.print().await?;
+
+	let req_list_b_tasks = hc.do_post(
+		"/api/rpc",
+		json!({
+		"jsonrpc": "2.0",
+			"id": 1,
+			"method": "list_tasks",
+			"params": {
+				"filters": [
+				{
+					"project_id": project_id,
+					"title": {"$contains": "BB"},
+				},
+				// Shows how to use other $in
+				{
+
+					"project_id": { "$in": [project_id] },
+					"title": {"$in": ["task AAA 3", "task AAA 4"]}
+				},
+				// This won't match any projects, so, won't return anything.
+				{
+					"project_id": { "$in": [ 123, 124]},
+					"title": {"$in": ["task AAA 2", "task AAA 5"]}
+				}
+				]
+			}
+		}),
+	);
+	req_list_b_tasks.await?.print().await?;
+
 	let req_logoff = hc.do_post(
 		"/api/logoff",
 		json!({
 			"logoff": true
 		}),
 	);
-	req_logoff.await?.print().await?;
+	// req_logoff.await?.print().await?;
 
 	Ok(())
 }
